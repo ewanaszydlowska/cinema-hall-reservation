bebaadbdd7bf3342280b50116b3bd375a01cd654
ewanaszydlowska <ewanasz@gmail.com>
2018-01-31 13:44:16 +0100

    add entity screening and seat, update cinema

--- src/main/java/pl/reservation/entity/Cinema.java
+++ src/main/java/pl/reservation/entity/Cinema.java
@@ -18,13 +18,13 @@ public class Cinema {
 
 	private String name;
 
-	private int numberOfAuditories;
-
 	private String address;
 
 	private String email;
 
 	private String phone;
+	
+	public Auditory auditory;
 
 	public Cinema() {
 		super();
@@ -34,50 +34,48 @@ public class Cinema {
 		return id;
 	}
 
-	public String getName() {
-		return name;
-	}
-	public int getNumberOfAuditories() {
-		return numberOfAuditories;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public String getName() {
+		return name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	public void setNumberOfAuditories(int numberOfAuditories) {
-		this.numberOfAuditories = numberOfAuditories;
+	public String getAddress() {
+		return address;
 	}
 
 	public void setAddress(String address) {
 		this.address = address;
 	}
 
+	public String getEmail() {
+		return email;
+	}
+
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
 	}
 
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 
-	
+	public Auditory getAuditory() {
+		return auditory;
+	}
 
-	
+	public void setAuditory(Auditory auditory) {
+		this.auditory = auditory;
+	}
+
 }
