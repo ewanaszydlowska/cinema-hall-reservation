554aa65c72bfddc3bbb5b6b739537e01b35b368f
ewanaszydlowska <ewanasz@gmail.com>
2018-01-31 13:34:07 +0100

    auditory entity added

---
+++ src/main/java/pl/reservation/entity/Auditory.java
@@ -0,0 +1,48 @@
+package pl.reservation.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "auditory")
+public class Auditory {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@NotNull
+	private Long id;
+	
+	private String name;
+	private int seatQuantity;
+	
+	public Auditory() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSeatQuantity() {
+		return seatQuantity;
+	}
+
+	public void setSeatQuantity(int seatQuantity) {
+		this.seatQuantity = seatQuantity;
+	}
+	
+}
