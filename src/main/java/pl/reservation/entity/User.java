489373e3c39dbd1e6a9c699b5470df4cb940f052
AdrianSzromba <adrian.szromba@gmail.com>
2018-02-01 13:28:07 +0100

    merge project

--- src/main/java/pl/reservation/entity/User.java
+++ src/main/java/pl/reservation/entity/User.java
@@ -111,7 +111,7 @@ public class User {
 	}
 
 	public void setPassword(String password) {
-		this.password = password;
+		this.password = BCrypt.hashpw(password, BCrypt.gensalt());
 	}
 	
 	public boolean isPasswordCorrect(String pwd) {
