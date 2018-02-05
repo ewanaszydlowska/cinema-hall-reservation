4e0921cc2c9c54970ee7a4e61a5f3fb86314ae6c
AdrianSzromba <adrian.szromba@gmail.com>
2018-02-05 14:21:43 +0100

    controller for view user details

--- src/main/java/pl/reservation/controller/UserViewController.java
+++ src/main/java/pl/reservation/controller/UserViewController.java
@@ -1,10 +1,25 @@
 package pl.reservation.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import pl.reservation.bean.SessionManager;
+import pl.reservation.entity.User;
+
+
 @Controller
-@RequestMapping("account")
+@RequestMapping("/account")
 public class UserViewController {
 
+	@GetMapping("")
+	public String myaccount(Model m) {
+		HttpSession s = SessionManager.session();
+		User u = (User) s.getAttribute("user");
+		m.addAttribute("user", u);
+		return "account/myaccount";
+	}
 }
