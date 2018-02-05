3de4490cb587b9edea27a1fe2e9ac8d02f15ea90
AdrianSzromba <adrian.szromba@gmail.com>
2018-02-05 12:49:16 +0100

    request mapping

--- src/main/java/pl/reservation/controller/AuditoryController.java
+++ src/main/java/pl/reservation/controller/AuditoryController.java
@@ -1,8 +1,10 @@
 package pl.reservation.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/auditory")
 public class AuditoryController {
 
 }
